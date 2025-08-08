9ddb907c804d98dcaef6986bd3c4815ccd4f7b0d
闫世杰 <shijieyan24@gmail.com>
2025-08-08 16:57:24 +0800

    创建后台管理模块

---
+++ gulimall-backend/src/main/java/com/atguigu/gulimall/bankend/BankendApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.bankend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname BankendApplication
+ * @Description
+ * @Date 2025/08/08 16:44
+ * @Created by YanShijie
+ */
+@SpringBootApplication
+public class BankendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankendApplication.class, args);
+    }
+}
